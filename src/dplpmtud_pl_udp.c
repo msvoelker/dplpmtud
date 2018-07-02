be73c6f6388db0234d0c9546cd304d50854bea89
msvoelker <timo.voelker@fh-muenster.de>
2018-07-02 13:07:15 +0200

    adjust too small probe_size

--- src/dplpmtud_pl_udp.c
+++ src/dplpmtud_pl_udp.c
@@ -23,9 +23,6 @@
 #define IP_HEADER_SIZE ((dplpmtud_ip_version == IPv4) ? IPv4_HEADER_SIZE : IPv6_HEADER_SIZE)
 #define UDP_HEADER_SIZE 8
 
-#define THREAD_NAME_SEND "dplpmtud_thread"
-#define THREAD_NAME_RECEIVE "listener_thread"
-
 struct udp_heartbeat {
 	uint8_t type;
 	uint8_t flags;
@@ -38,16 +35,16 @@ static uint32_t token = 4711;
 
 // message specific 
 int send_probe(int socket, uint32_t probe_size) {
-	LOG_DEBUG_("%s - send_probe entered", THREAD_NAME_SEND);
-	//size_t probe_size = 1300;
-	
-	// assert(probe_size > IPv4_HEADER_SIZE + 6)
+	LOG_DEBUG_("%s - send_probe entered", THREAD_NAME);
 	
 	char *udp_payload;
 	struct udp_heartbeat *heartbeat_request;
 	size_t udp_payload_size;
 	
 	udp_payload_size = probe_size - IP_HEADER_SIZE - UDP_HEADER_SIZE;
+	if (udp_payload_size < 0) {
+		udp_payload_size = 0;
+	}
 	udp_payload = malloc(udp_payload_size);
 	memset(udp_payload, 0, udp_payload_size);
 	
@@ -57,7 +54,7 @@ int send_probe(int socket, uint32_t probe_size) {
 	heartbeat_request->seq_no = get_probe_sequence_number();
 	heartbeat_request->token = token;
 	
-	LOG_DEBUG_("%s - leave send_probe", THREAD_NAME_SEND);
+	LOG_DEBUG_("%s - leave send_probe", THREAD_NAME);
 	return send(socket, (const void *) udp_payload, udp_payload_size, 0);
 }
 
@@ -66,21 +63,21 @@ static int handle_heartbeat_response(struct udp_heartbeat *heartbeat_response) {
 	uint32_t seq_no;
 	
 	seq_no = get_probe_sequence_number();
-	LOG_DEBUG_("%s - handle_heartbeat_response entered", THREAD_NAME_RECEIVE);
-	LOG_DEBUG_("%s - heartbeat_response->token: %u, token: %u heartbeat_response->seq_no: %u, probe_sequence_number: %u", THREAD_NAME_RECEIVE, heartbeat_response->token, token, heartbeat_response->seq_no, seq_no);
+	LOG_DEBUG_("%s - handle_heartbeat_response entered", THREAD_NAME);
+	LOG_DEBUG_("%s - heartbeat_response->token: %u, token: %u heartbeat_response->seq_no: %u, probe_sequence_number: %u", THREAD_NAME, heartbeat_response->token, token, heartbeat_response->seq_no, seq_no);
 	if (heartbeat_response->token == token && heartbeat_response->seq_no == seq_no) {
 		signal_probe_return();
-		LOG_DEBUG_("%s - leave handle_heartbeat_response", THREAD_NAME_RECEIVE);
+		LOG_DEBUG_("%s - leave handle_heartbeat_response", THREAD_NAME);
 		return 1;
 	}
-	LOG_DEBUG_("%s - leave handle_heartbeat_response", THREAD_NAME_RECEIVE);
+	LOG_DEBUG_("%s - leave handle_heartbeat_response", THREAD_NAME);
 	return 0;
 }
 
 
 // message specific 
 static int send_heartbeat_response(struct udp_heartbeat *heartbeat_request, int socket, struct sockaddr *to_addr, socklen_t to_addr_len) {
-	LOG_DEBUG_("%s - send_heartbeat_response entered", THREAD_NAME_RECEIVE);
+	LOG_DEBUG_("%s - send_heartbeat_response entered", THREAD_NAME);
 	struct udp_heartbeat heartbeat_respone;
 	
 	memset(&heartbeat_respone, 0, sizeof(struct udp_heartbeat));
@@ -89,40 +86,40 @@ static int send_heartbeat_response(struct udp_heartbeat *heartbeat_request, int 
 	heartbeat_respone.seq_no = heartbeat_request->seq_no;
 	heartbeat_respone.token = heartbeat_request->token;
 	
-	LOG_DEBUG_("%s - leave send_heartbeat_response", THREAD_NAME_RECEIVE);
+	LOG_DEBUG_("%s - leave send_heartbeat_response", THREAD_NAME);
 	return sendto(socket, (const void *)&heartbeat_respone, sizeof(struct udp_heartbeat), 0, to_addr, to_addr_len);
 }
 
 // message specific 
 int message_handler(int socket, void *message, size_t message_length, struct sockaddr *from_addr, socklen_t from_addr_len) {
-	LOG_DEBUG_("%s - message_handler entered", THREAD_NAME_RECEIVE);
+	LOG_DEBUG_("%s - message_handler entered", THREAD_NAME);
 	struct udp_heartbeat *heartbeat;
 	
 	if (message_length < 12) {
-		LOG_ERROR_("%s - message is too small for a heartbeat", THREAD_NAME_RECEIVE);
-		LOG_DEBUG_("%s - leave message_handler", THREAD_NAME_RECEIVE);
+		LOG_ERROR_("%s - message is too small for a heartbeat", THREAD_NAME);
+		LOG_DEBUG_("%s - leave message_handler", THREAD_NAME);
 		return -1;
 	}
 	heartbeat = (struct udp_heartbeat *)message;
 	
 	if (ntohs(heartbeat->length) != 12) {
-		LOG_ERROR_("%s - heartbeat length is not 12 byte. %hu", THREAD_NAME_RECEIVE, heartbeat->length);
-		LOG_DEBUG_("%s - leave message_handler", THREAD_NAME_RECEIVE);
+		LOG_ERROR_("%s - heartbeat length is not 12 byte. %hu", THREAD_NAME, heartbeat->length);
+		LOG_DEBUG_("%s - leave message_handler", THREAD_NAME);
 		return -1;
 	}
 	
-	LOG_DEBUG_("%s - heartbeat type: %u, length: %u received", THREAD_NAME_RECEIVE, heartbeat->type, ntohs(heartbeat->length));
+	LOG_DEBUG_("%s - heartbeat type: %u, length: %u received", THREAD_NAME, heartbeat->type, ntohs(heartbeat->length));
 	if (heartbeat->type == 4) {
 		// heartbeat request
-		LOG_DEBUG_("%s - leave message_handler", THREAD_NAME_RECEIVE);
+		LOG_DEBUG_("%s - leave message_handler", THREAD_NAME);
 		return send_heartbeat_response(heartbeat, socket, from_addr, from_addr_len);
 	} else if (heartbeat->type == 5) {
 		// valid heartbeat response
-		LOG_DEBUG_("%s - leave message_handler", THREAD_NAME_RECEIVE);
+		LOG_DEBUG_("%s - leave message_handler", THREAD_NAME);
 		return handle_heartbeat_response(heartbeat);
 	}
 	
-	LOG_DEBUG_("%s - leave message_handler", THREAD_NAME_RECEIVE);
+	LOG_DEBUG_("%s - leave message_handler", THREAD_NAME);
 	return -1;
 	
 }
